0e162222efc38e572b7dc9b750ba4e65242f5296
Deepankar <deepankarsalhan@gmail.com>
2024-02-06 06:54:09 -0700

    updated modal

--- components/Library.tsx
+++ components/Library.tsx
@@ -1,11 +1,23 @@
 'use client';
 
+import useAuthModal from '@/hooks/useAuthModal';
+import useUploadModal from '@/hooks/useUploadModal';
+import { useUser } from '@/hooks/useUser';
 import { AiOutlinePlus } from 'react-icons/ai';
 import { TbPlaylist } from 'react-icons/tb';
 
 const Library = () => {
+  const authModal = useAuthModal();
+  const uploadModal = useUploadModal();
+  const { user } = useUser();
+
   const onClick = () => {
-    //Handle upload later
+    if (!user) {
+      return authModal.onOpen();
+    }
+
+    // TODO: Check for subscription
+    return uploadModal.onOpen();
   };
   return (
     <div className='flex flex-col'>
